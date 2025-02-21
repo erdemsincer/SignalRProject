4614b0b7af20e90566efc511a6a004438359668b
Erdem <erdemsincer123@icloud.com>
2025-02-21 13:36:16 +0300

    Category controller

--- SignalRApi/Controllers/CategoryController.cs
+++ SignalRApi/Controllers/CategoryController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -7,5 +11,60 @@ namespace SignalRApi.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            Category about = new Category()
+            {
+                Status = createCategoryDto.Status,
+                CategoryName = createCategoryDto.CategoryName
+                
+
+            };
+            _categoryService.TAdd(about);
+            return Ok("eklendi");
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            Category about = new Category()
+            {
+                CategoryId = updateCategoryDto.CategoryId,
+                CategoryName = updateCategoryDto.CategoryName,
+                Status = updateCategoryDto.Status,
+
+
+
+            };
+
+            _categoryService.TUpdate(about);
+            return Ok("Güncellendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            _categoryService.TDelete(value);
+            return Ok("Silindi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByIdCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            return Ok(value);
+        }
     }
 }
