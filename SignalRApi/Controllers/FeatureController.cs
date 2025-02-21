d4f6dd331228aebbd580130cb9d00b48d32e1b06
Erdem <erdemsincer123@icloud.com>
2025-02-21 13:50:13 +0300

    Feature controller

---
+++ SignalRApi/Controllers/FeatureController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.FeatureDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IFeatureService _featureService;
+
+        public FeatureController(IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _featureService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            Feature about = new Feature()
+            {
+                Description1 = createFeatureDto.Description1,
+                Description2 = createFeatureDto.Description2,
+                Description3 = createFeatureDto.Description3,
+                Title1 = createFeatureDto.Title1,
+                Title2 = createFeatureDto.Title2,
+                Title3 = createFeatureDto.Title3
+
+
+            };
+            _featureService.TAdd(about);
+            return Ok("eklendi");
+        }
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            Feature about = new Feature()
+            {
+                FeatureId = updateFeatureDto.FeatureId,
+                Description1 = updateFeatureDto.Description1,
+                Description2 = updateFeatureDto.Description2,
+                Description3 = updateFeatureDto.Description3,
+                Title1 = updateFeatureDto.Title1,
+                Title2 = updateFeatureDto.Title2,
+                Title3 = updateFeatureDto.Title3
+            };
+
+            _featureService.TUpdate(about);
+            return Ok("Güncellendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _featureService.TGetById(id);
+            _featureService.TDelete(value);
+            return Ok("Silindi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByIdFeature(int id)
+        {
+            var value = _featureService.TGetById(id);
+            return Ok(value);
+        }
+    }
+}
