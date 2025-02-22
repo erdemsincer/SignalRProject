059fb4e237602cadde0ee4a93cd12db0304a09df
Erdem <erdemsincer123@icloud.com>
2025-02-22 14:58:40 +0300

    UI KATMANINDAKİ DTOLAR TAMAMLANDI

---
+++ SignalRWebUI/Dtos/SocialMediaDtos/CreateSocialMediaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.SocialMediaDtos
+{
+    public class CreateSocialMediaDto
+    {
+       
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string İcon { get; set; }
+    }
+}
---
+++ SignalRWebUI/Dtos/SocialMediaDtos/UpdateSocialMediaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.SocialMediaDtos
+{
+    public class UpdateSocialMediaDto
+    {
+        public int SocialMediaId { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string İcon { get; set; }
+    }
+}
