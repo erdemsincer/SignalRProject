9680ee796dab94c148c6be0e98b5879855d67280
Erdem <erdemsincer123@icloud.com>
2025-02-22 14:24:24 +0300

    Product ekleme işlemleri

--- SignalRWebUI/Controllers/ProductController.cs
+++ SignalRWebUI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
 using SignalRWebUI.Dtos.ProductDtos;
 
 namespace SignalRWebUI.Controllers
@@ -23,6 +25,22 @@ namespace SignalRWebUI.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsondata);
                 return View(values);
             }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7244/api/Category");
+            var  jsonData= await responseMessage.Content.ReadAsStringAsync();
+            var values= JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.CategoryName,
+                                                Value = x.CategoryId.ToString()
+                                            }).ToList();
+            ViewBag.v=values2;
             return View();
         }
     }
