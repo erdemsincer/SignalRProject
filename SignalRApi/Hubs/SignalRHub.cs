f2acc299d769c289349f54f573ba34702836d202
Erdem <erdemsincer123@icloud.com>
2025-02-26 00:26:07 +0300

    istatistikler signalr ile getirme işlemi

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -17,15 +17,30 @@ namespace SignalRApi.Hubs
 
 
 
-		public async Task SendCategoryCount() 
+		public async Task SendStatistic() 
         {
             var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
-        }
-		public async Task SendProductCount()
-		{
+
 			var value2 = _productService.TProductCount();
 			await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+			var value3 = _categoryService.TActiveCategoryCount();
+			await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+			var value4 = _categoryService.TPassiveCategoryCount();
+			await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+			
+			var value5=_productService.TProductCountByCategoryNameHamburger();
+			await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburgerCount", value5);
+
+			var value6 = _productService.TProductCountByCategoryNameDrink();
+			await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrinkCount", value6);
+
+			var value7 = _productService.TProductPriceAvg();
+			await Clients.All.SendAsync("ReceiveProductPriceAvg", value7);
+
 		}
+		
 	}
 }
