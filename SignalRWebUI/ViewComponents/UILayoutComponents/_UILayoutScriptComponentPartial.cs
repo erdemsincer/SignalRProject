7e5f3090a3e007771c1289473bbdd511d7a2b863
Erdem <erdemsincer123@icloud.com>
2025-02-27 00:55:37 +0300

    Template parçalandı

---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutScriptComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScriptComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
