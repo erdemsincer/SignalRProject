acaf65b1e08211533817a892b75869c780520a54
Erdem <erdemsincer123@icloud.com>
2025-02-26 21:53:08 +0300

    en pahalı ürün istatistiği

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -38,7 +38,10 @@ namespace SignalRApi.Hubs
 			await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrinkCount", value6);
 
 			var value7 = _productService.TProductPriceAvg();
-			await Clients.All.SendAsync("ReceiveProductPriceAvg", value7);
+			await Clients.All.SendAsync("ReceiveProductPriceAvg", value7.ToString("0.00")+"TL");
+
+			var value8 = _productService.TProductNameByMaxPrice();
+			await Clients.All.SendAsync("ReceiveProductNameByMaxPrice", value8);
 
 		}
 		
